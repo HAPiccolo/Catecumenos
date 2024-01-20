b6e1c1526555591b400be9f3437276bcc576f454
HAPiccolo <hapiccolo86@gmail.com>
2024-01-20 15:40:36 -0300

    mejora en la logica, add new user

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .models import Alumno, Tutor, Curso, Docente, Direccion
 from .exceptions import AlumnoExistException
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+
 
 # Create your views here.
 
@@ -104,39 +108,38 @@ def registrar(request):
         depto = request.POST["depto"]
         sector = request.POST["sector"]
 
+        # Busca un tutor existente o crea uno nuevo
+        tutor, created = Tutor.objects.get_or_create(
+            dni_tutor=dni_tutor,
+            defaults={'apellido_tutor': apellido_tutor,
+                      'nombre_tutor': nombre_tutor, 'telefono_tutor': telefono_tutor}
+        )
+
+        # Si el tutor ya existía, actualiza los datos del tutor
+        if not created:
+            tutor.apellido_tutor = apellido_tutor
+            tutor.nombre_tutor = nombre_tutor
+            tutor.telefono_tutor = telefono_tutor
+            tutor.save()
+
         try:
             # Verifica si el alumno ya existe con el mismo tutor
             alumno_existente = Alumno.objects.filter(
-                dni_alumno=dni_alumno, tutor__dni_tutor=dni_tutor).first()
+                dni_alumno=dni_alumno, tutor=tutor).first()
 
             if alumno_existente:
-                raise AlumnoExistException(
-                    "El alumno ya existe con este tutor.")
-
-            # Busca un tutor existente o crea uno nuevo
-            tutor, created = Tutor.objects.get_or_create(
-                dni_tutor=dni_tutor,
-                defaults={'apellido_tutor': apellido_tutor,
-                          'nombre_tutor': nombre_tutor, 'telefono_tutor': telefono_tutor}
-            )
-
-            # Si el tutor ya existía, actualiza los datos del tutor
-            if not created:
-                tutor.apellido_tutor = apellido_tutor
-                tutor.nombre_tutor = nombre_tutor
-                tutor.telefono_tutor = telefono_tutor
-                tutor.save()
-
+                raise AlumnoExistException("El alumno ya existe con este tutor.")
+            else:
                 alumno = Alumno.objects.create(dni_alumno=dni_alumno, apellido_alumno=apellido_alumno,
-                                            nombre_alumno=nombre_alumno, curso_alumno=nombre_curso, tutor=tutor)
+                                               nombre_alumno=nombre_alumno, curso_alumno=nombre_curso, tutor=tutor)
 
                 direccion = Direccion.objects.create(barrio=barrio, vivienda=vivienda, calle=calle,
-                                                    piso=piso, depto=depto, sector=sector, dni_tutor=tutor)
+                                                     piso=piso, depto=depto, sector=sector, dni_tutor=tutor)
 
                 alumno.save()
                 direccion.save()
 
-            return render(request, 'core/sucess_save.html')
+                return render(request, 'core/sucess_save.html')
 
         except AlumnoExistException as e:
             return render(request, 'core/error_save.html')
@@ -150,3 +153,97 @@ def registrar(request):
             'docentes': docentes,
         }
         return render(request, 'core/registrar.html', datos)
+
+
+def cursos(request):
+    # Se obtienen los cursos
+    cursos = Curso.objects.all()
+
+    # Si hay una peticion la comprueba
+    if request.method == "POST":
+        add_curso = request.POST['nombre_curso']
+        if Curso.objects.filter(nombre_curso=add_curso).exists():
+            print("Ya existe")
+        else:
+            Curso.objects.create(nombre_curso=add_curso)
+
+    datos = {
+        'cursos': cursos,
+    }
+    # datos: carga los cursos al cargar la pagina
+    return render(request, 'core/cursos.html', datos)
+
+# FUNCION QUE ELIMINA LOS CURSOS
+
+
+def eliminar_curso(request, curso_id):
+
+    # Obtiene el id del curso que se desea eliminar
+    curso = Curso.objects.get(id=curso_id)
+    # Elimina el curso
+    curso.delete()
+    return redirect('cursos')  # Redirecciona a la pagina cursos
+
+
+def docentes(request):
+   
+    docentes = Docente.objects.all()
+
+    # Si hay una peticion la comprueba
+    if request.method == "POST":
+        dni_docente = request.POST['dni_docente']
+        apellido_docente = request.POST['apellido_docente']
+        nombre_docente = request.POST['nombre_docente']
+        telefono_docente = request.POST['telefono_docente']
+
+        if Docente.objects.filter(dni_docente=dni_docente).exists():
+            return render(request, 'core/docentes.html', {'dni_duplicado': True, 'docentes':docentes})
+        else:
+            Docente.objects.create(dni_docente=dni_docente, apellido_docente=apellido_docente,
+                                   nombre_docente=nombre_docente, telefono_docente=telefono_docente)
+
+    datos = {
+        'docentes': docentes,
+    }
+    # datos: carga los cursos al cargar la pagina
+    return render(request, 'core/docentes.html', datos)
+
+
+# FUNCION PARA ELIMINAR DOCENTES
+def eliminar_docente(request, docente_id):
+
+    docente = Docente.objects.get(id=docente_id)
+    docente.delete()
+    return redirect('docentes')  # Redirecciona a la pagina cursos
+
+def usuarios(request):
+    # PERMITE REGISTRAR UN NUEVO USUARIO    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            # Asignamos el grupo al usuario
+
+            group_name = 'dataentry'
+            group, created = Group.objects.get_or_create(name=group_name)
+            user.groups.add(group)
+
+            return redirect('usuarios')
+    else:
+        form = UserCreationForm()
+
+    form.fields['username'].label = "Nombre de usuario"
+    form.fields['password1'].help_text = "Una contraseña fuerte es recomendada"
+    
+    
+
+    return render(request, 'core/usuarios.html', {'form':form})
+
+def asignacion_cursos(request):
+
+    
+    return render(request, 'core/asignacion_cursos.html')
+
+
+
